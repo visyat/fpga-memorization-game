abf53dca0d170a30c399de01059f7237e503e802
Vishal Yathish <vishal.yathish@gmail.com>
2024-11-25 09:26:50 -0800

    checking user input

--- randnum.v
+++ randnum.v
@@ -5,10 +5,31 @@ module randnum (
     input rst;
     output reg [13:0] randInt;
 
+    reg [13:0] storeRand;
+
     always (*) begin
         if (rst) begin
-            randInt = $urandom%9999;
+            storeRand = $urandom%9999;
+        end
+        randInt = storeRand;
+    end
+endmodule
+
+module checkInput(
+    userInt,
+    randInt,
+    correct
+);
+    input [13:0] userInt;
+    input [13:0] randInt; 
+    
+    output reg correct;
+
+    always(*) begin
+        if (userInt == randInt) begin
+            correct <= 1;
+        end else begin
+            correct <= 0;
         end
     end
-
 endmodule
